d98ac60675336e354e83f374877b244f00a9cd05
22deer44567890 <lgolub110@gmail.com>
2024-06-06 14:42:17 +0300

    sss+

--- testProject/testProject/Form1.Designer.cs
+++ testProject/testProject/Form1.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ыыы = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ыыы
+            // 
+            this.ыыы.Location = new System.Drawing.Point(507, 0);
+            this.ыыы.Name = "ыыы";
+            this.ыыы.Size = new System.Drawing.Size(294, 216);
+            this.ыыы.TabIndex = 0;
+            this.ыыы.Text = "ыыы";
+            this.ыыы.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ыыы);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ыыы;
     }
 }
 
